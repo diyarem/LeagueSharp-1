5a20dbbb436dbdaf8b2346c6d7455477def43f7c
RoachxD <roachxxxx@gmail.com>
2015-02-27 00:27:55 +0200

    "Change is good!"  - Fixed for Latest L# Version.  - Loading Assembly Async.  - Removed Unused References.  - Removed Unused Callbacks/Events.  - Re-indented the Code.  - Improved anything I could (Code).

--- KhazixSharpElite/Khazix.cs
+++ KhazixSharpElite/Khazix.cs
@@ -1,53 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Net.Mime;
-using System.Runtime.InteropServices;
 using LeagueSharp;
 using LeagueSharp.Common;
-using System.Drawing;
 using SharpDX;
 
 namespace KhazixSharp
 {
-    class Khazix
+    internal class Khazix
     {
-        public static Obj_AI_Hero Player = ObjectManager.Player;
-
-        public static Spellbook sBook = Player.Spellbook;
-
-        public static Orbwalking.Orbwalker orbwalker;
-
-        public static SpellDataInst Qdata = sBook.GetSpell(SpellSlot.Q);
-        public static SpellDataInst Wdata = sBook.GetSpell(SpellSlot.W);
-        public static SpellDataInst Edata = sBook.GetSpell(SpellSlot.E);
-        public static SpellDataInst Rdata = sBook.GetSpell(SpellSlot.R);
+        public static Orbwalking.Orbwalker Orbwalker;
         public static Spell Q = new Spell(SpellSlot.Q, 325);
         public static Spell W = new Spell(SpellSlot.W, 1000);
         public static Spell E = new Spell(SpellSlot.E, 700);
         public static Spell R = new Spell(SpellSlot.R, 0);
+        public static SummonerItems SumItems;
 
-
-
-
-        public static SummonerItems sumItems;
-
-        public static void setSkillshots()
+        public static void SetSkillshots()
         {
             W.SetSkillshot(0.225f, 100f, 828.5f, true, SkillshotType.SkillshotLine);
             E.SetSkillshot(0.250f, 100f, 1000f, false, SkillshotType.SkillshotCircle);
-            sumItems = new SummonerItems(Player);
+            SumItems = new SummonerItems(Player);
         }
 
-        public static void checkUpdatedSpells()
+        public static void CheckUpdatedSpells()
         {
             if (Qdata.Name == "khazixqlong")
+            {
                 Q.Range = 375;
-            if (Edata.Name == "khazixelong")
-                E.Range = 1000;
+            }
 
-               /* foreach(PropertyDescriptor descriptor in TypeDescriptor.GetProperties(Rdata))
+            if (Edata.Name == "khazixelong")
+            {
+                E.Range = 1000;
+            }
+
+            /* foreach(PropertyDescriptor descriptor in TypeDescriptor.GetProperties(Rdata))
                 {
                     string name=descriptor.Name;
                     object value = descriptor.GetValue(Rdata);
@@ -55,201 +42,248 @@ namespace KhazixSharp
                 }*/
         }
 
-
-        public static void doCombo(Obj_AI_Base target)
+        public static void DoCombo(Obj_AI_Base target)
         {
             if (target == null || !target.IsValidTarget())
+            {
                 return;
+            }
 
             if (target.Distance(Player) < 500)
             {
-                sumItems.cast(SummonerItems.ItemIds.Ghostblade);
+                SumItems.cast(SummonerItems.ItemIds.Ghostblade);
             }
-            if (target.Distance(Player) < 500 && (Player.Health / Player.MaxHealth) * 100 < 85)
-            {
-                sumItems.cast(SummonerItems.ItemIds.BotRK, target);
 
+            if (target.Distance(Player) < 500 && (Player.Health/Player.MaxHealth)*100 < 85)
+            {
+                SumItems.cast(SummonerItems.ItemIds.BotRk, target);
             }
-            if (isStealthed() && targIsKillabe(target) && enemiesNear() > 2)
+
+            if (IsStealthed() && TargIsKillabe(target) && EnemiesNear() > 2)
             {
                 Orbwalking.Attack = false;
             }
             else
             {
-                useHydra(target);
-                doQ(target);
+                UseHydra(target);
+                DoQ(target);
                 Orbwalking.Attack = true;
-                doSmartW(target);
-                if (target.Health < fullComboDmgOn(target) * 1.3f || isStealthed())
-                    doSmartE(target, true);
+                DoSmartW(target);
+                if (target.Health < FullComboDmgOn(target)*1.3f || IsStealthed())
+                {
+                    DoSmartE(target, true);
+                }
                 else
-                    reachWithE(target);
+                {
+                    ReachWithE(target);
+                }
             }
-            doSmartR(target);
+            DoSmartR(target);
         }
 
-        public static void doHarass(Obj_AI_Base target)
+        public static void DoHarass(Obj_AI_Base target)
         {
             if (target == null || !target.IsValidTarget())
+            {
                 return;
+            }
 
-                useHydra(target);
-                doQ(target);
-                doSmartW(target);
+            UseHydra(target);
+            DoQ(target);
+            DoSmartW(target);
         }
 
-        public static void doQ(Obj_AI_Base target)
+        public static void DoQ(Obj_AI_Base target)
         {
-            if (inSpellRange(target, Q) && Q.IsReady())
+            if (InSpellRange(target, Q) && Q.IsReady())
+            {
                 Q.Cast(target);
+            }
         }
 
-        public static void doSmartW(Obj_AI_Base target)
+        public static void DoSmartW(Obj_AI_Base target)
         {
             if (!W.IsReady() || !target.IsValidTarget())
+            {
                 return;
-            PredictionOutput po = W.GetPrediction(target);
+            }
+
+            var po = W.GetPrediction(target);
             if (po.Hitchance > HitChance.Low)
+            {
                 W.Cast(po.CastPosition);
+            }
         }
 
-        public static void doSmartE(Obj_AI_Base target,bool kill = false)
+        public static void DoSmartE(Obj_AI_Base target, bool kill = false)
         {
-            if (!E.IsReady() || timeToReachAA(target) < 0.3f || !target.IsValidTarget())
+            if (!E.IsReady() || TimeToReachAa(target) < 0.3f || !target.IsValidTarget())
+            {
                 return;
+            }
 
             Console.WriteLine("do some jumpy");
-            PredictionOutput po = E.GetPrediction(target);
+            var po = E.GetPrediction(target);
             if (po.Hitchance > HitChance.Medium)
-                E.Cast((kill)?po.CastPosition:po.UnitPosition);
+            {
+                E.Cast((kill) ? po.CastPosition : po.UnitPosition);
+            }
         }
 
-        public static void reachWithE(Obj_AI_Base target)
+        public static void ReachWithE(Obj_AI_Base target)
         {
             if (!E.IsReady())
+            {
                 return;
-            float trueAARange = Player.AttackRange + target.BoundingRadius;
-            float trueERange = target.BoundingRadius + E.Range;
+            }
 
-            float dist = Player.Distance(target);
-            
-
-            float timeToReach = timeToReachAA(target);
-            if (dist > trueAARange && (dist < trueERange ))
+            var trueAaRange = Player.AttackRange + target.BoundingRadius;
+            var trueERange = target.BoundingRadius + E.Range;
+            var dist = Player.Distance(target);
+            var timeToReach = TimeToReachAa(target);
+            if (!(dist > trueAaRange) || (!(dist < trueERange)))
             {
-                if (timeToReach > 2.2f)
-                {
-                    doSmartE(target);
-                }
+                return;
+            }
+
+            if (timeToReach > 2.2f)
+            {
+                DoSmartE(target);
             }
         }
 
-        public static float timeToReachAA(Obj_AI_Base target)
+        public static float TimeToReachAa(Obj_AI_Base target)
         {
-            float trueAARange = Player.AttackRange + target.BoundingRadius;
-
-            float dist = Player.Distance(target);
-            Vector2 walkPos = new Vector2();
-            if (target.IsMoving && target.Path.Count()>0)
+            var trueAaRange = Player.AttackRange + target.BoundingRadius;
+            var dist = Player.Distance(target);
+            var walkPos = new Vector2();
+            if (target.IsMoving && target.Path.Count() > 0)
             {
-                Vector2 tpos = target.Position.To2D();
-                Vector2 path = target.Path[0].To2D() - tpos;
+                var tpos = target.Position.To2D();
+                var path = target.Path[0].To2D() - tpos;
                 path.Normalize();
-                walkPos = tpos + (path * 100);
+                walkPos = tpos + (path*100);
             }
-            float targ_ms = (target.IsMoving && Player.Distance(walkPos) > dist) ? target.MoveSpeed : 0;
-            float msDif = (Player.MoveSpeed - targ_ms) == 0 ? 0.0001f : (Player.MoveSpeed - targ_ms);
-            float timeToReach = (dist - trueAARange) / msDif;
+            var targMs = (target.IsMoving && Player.Distance(walkPos) > dist) ? target.MoveSpeed : 0;
+            var msDif = (Player.MoveSpeed - targMs) == 0 ? 0.0001f : (Player.MoveSpeed - targMs);
+            var timeToReach = (dist - trueAaRange)/msDif;
 
-            return (timeToReach>=0)?timeToReach:float.MaxValue;
+            return (timeToReach >= 0) ? timeToReach : float.MaxValue;
         }
 
-        public static void doSmartR(Obj_AI_Base target)
+        public static void DoSmartR(Obj_AI_Base target)
         {
             if (!R.IsReady())
-                return;
-            var dist = Player.Distance(target);
-            if (enemiesNear() > 2 || !gotPassiveDmg() || (timeToReachAA(target) > 1f && targIsKillabe(target)))
-                if (Player.Distance(target) < 375 && (!Q.IsReady() || E.IsReady()) && (dist> Q.Range || !Q.IsReady()))
-                {
-                    R.Cast();
-                }
-        }
-
-        public static bool useHydra(Obj_AI_Base target)
-        {
-            if (target.Distance(Player.ServerPosition) < (400 + target.BoundingRadius - 20))
             {
-                Items.UseItem(3074, target);
-                Items.UseItem(3077, target);
-                return true;
+                return;
             }
-            return false;
+
+            var dist = Player.Distance(target);
+            if (EnemiesNear() <= 2 && GotPassiveDmg() && (!(TimeToReachAa(target) > 1f) || !TargIsKillabe(target)))
+            {
+                return;
+            }
+
+            if (Player.Distance(target) < 375 && (!Q.IsReady() || E.IsReady()) && (dist > Q.Range || !Q.IsReady()))
+            {
+                R.Cast();
+            }
         }
 
-        public static float getBestRange()
+        public static bool UseHydra(Obj_AI_Base target)
+        {
+            if (!(target.Distance(Player.ServerPosition) < (400 + target.BoundingRadius - 20)))
+            {
+                return false;
+            }
+
+            Items.UseItem(3074, target);
+            Items.UseItem(3077, target);
+            return true;
+        }
+
+        public static float GetBestRange()
         {
             if (E.IsReady())
+            {
                 return E.Range + Orbwalking.GetRealAutoAttackRange(Player);
-            if (W.IsReady())
-                return W.Range;
-            return Q.Range;
+            }
+
+            return W.IsReady() ? W.Range : Q.Range;
         }
 
-        public static float fullComboDmgOn(Obj_AI_Base target)
+        public static float FullComboDmgOn(Obj_AI_Base target)
         {
-            float dmg = 0f;
-            if (gotPassiveDmg())
+            var dmg = 0f;
+            if (GotPassiveDmg())
+            {
                 dmg +=
                     (float)
                         Player.CalcDamage(target, Damage.DamageType.Magical,
-                            10 + 10*Player.Level + 0.5*Player.FlatMagicDamageMod); //DamageLib.CalcMagicDmg(10 + 10* Player.Level + 0.5*Player.FlatMagicDamageMod,target);
+                            10 + 10*Player.Level + 0.5*Player.FlatMagicDamageMod);
+                // DamageLib.CalcMagicDmg(10 + 10* Player.Level + 0.5*Player.FlatMagicDamageMod,target);
+            }
+
             if (Q.IsReady())
             {
-                if (targetIsIsolated(target))
-                    dmg += Q.GetDamage(target, 1);// DamageLib.getDmg(target, DamageLib.SpellType.Q, DamageLib.StageType.FirstDamage);
+                if (TargetIsIsolated(target))
+                {
+                    dmg += Q.GetDamage(target, 1);
+                    // DamageLib.getDmg(target, DamageLib.SpellType.Q, DamageLib.StageType.FirstDamage);
+                }
                 else
+                {
                     dmg += Q.GetDamage(target);
+                }
             }
-            if (W.IsReady())
-                dmg += W.GetDamage(target);
-            if(E.IsReady())
-                dmg += E.GetDamage(target);
 
+            if (W.IsReady())
+            {
+                dmg += W.GetDamage(target);
+            }
+
+            if (E.IsReady())
+            {
+                dmg += E.GetDamage(target);
+            }
 
             return dmg;
         }
 
-        public static bool targIsKillabe(Obj_AI_Base target)
+        public static bool TargIsKillabe(Obj_AI_Base target)
         {
-            return target.Health > fullComboDmgOn(target)*1.2f;
+            return target.Health > FullComboDmgOn(target)*1.2f;
         }
-        public static bool gotPassiveDmg()
+
+        public static bool GotPassiveDmg()
         {
             return Player.Buffs.Any(buf => buf.Name == "khazixpdamage");
         }
 
-        public static bool isStealthed()
+        public static bool IsStealthed()
         {
             return Player.Buffs.Any(buf => buf.Name == "khazixrstealth");
         }
 
-        public static int enemiesNear()
+        public static int EnemiesNear()
         {
             return ObjectManager.Get<Obj_AI_Base>().Count(ene => ene.IsEnemy && ene.IsValidTarget(600));
         }
 
-        public static float targIsReach(Obj_AI_Base target)
+        public static float TargIsReach(Obj_AI_Base target)
         {
-            float dist = target.Distance(Player);
-            float range = Orbwalking.GetRealAutoAttackRange(target);
+            var dist = target.Distance(Player);
+            var range = Orbwalking.GetRealAutoAttackRange(target);
             if (Q.IsReady())
+            {
                 range = Q.Range;
-            range+= E.Range;
-            return dist-range;
+            }
+
+            range += E.Range;
+            return dist - range;
         }
 
-        public static bool targetIsIsolated(Obj_AI_Base target)
+        public static bool TargetIsIsolated(Obj_AI_Base target)
         {
             var enes = ObjectManager.Get<Obj_AI_Base>()
                 .Where(her => her.IsEnemy && her.NetworkId != target.NetworkId && target.Distance(her) < 500)
@@ -257,12 +291,18 @@ namespace KhazixSharp
             return !enes.Any();
         }
 
-        public static bool inSpellRange(Obj_AI_Base target, Spell S)
+        public static bool InSpellRange(Obj_AI_Base target, Spell s)
         {
-            var targBB = target.BoundingRadius*0.9f;
+            var targBb = target.BoundingRadius*0.9f;
             var dist = Vector3.DistanceSquared(target.Position, Player.Position);
-            return dist < (targBB + S.Range)*(targBB + S.Range);
+            return dist < (targBb + s.Range)*(targBb + s.Range);
         }
 
+        public static Obj_AI_Hero Player = ObjectManager.Player;
+        public static Spellbook SBook = Player.Spellbook;
+        public static SpellDataInst Qdata = SBook.GetSpell(SpellSlot.Q);
+        public static SpellDataInst Wdata = SBook.GetSpell(SpellSlot.W);
+        public static SpellDataInst Edata = SBook.GetSpell(SpellSlot.E);
+        public static SpellDataInst Rdata = SBook.GetSpell(SpellSlot.R);
     }
 }
