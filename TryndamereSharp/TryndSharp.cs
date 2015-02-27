8606fb575b43f44c5eb38bda0cb798f3c80b868b
RoachxD <roachxxxx@gmail.com>
2015-02-27 13:39:12 +0200

    "My right arm is a lot stronger than my left arm!"  - Updated for Latest L# Version.  - Re-indented the whole Assembly.  - Loading the Assembly Async.  - Removed Unused References.  - Removed Unused Events/Functions and Snippets of Code.

--- TryndamereSharp/TryndSharp.cs
+++ TryndamereSharp/TryndSharp.cs
@@ -1,149 +1,124 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Drawing;
+using System.Threading;
 using LeagueSharp;
 using LeagueSharp.Common;
-using System.Drawing;
 
 /*
  * ToDo:
  * 
  * */
 
-
 namespace TryndSharp
 {
     internal class TryndSharp
     {
-
         public const string CharName = "Tryndamere";
-
         public static Menu Config;
-
-        public static Obj_AI_Hero target;
+        public static Obj_AI_Hero Target;
 
         public TryndSharp()
         {
             /* CallBAcks */
-            CustomEvents.Game.OnGameLoad += onLoad;
-
+            CustomEvents.Game.OnGameLoad += delegate
+            {
+                var onGameLoad = new Thread(Game_OnGameLoad);
+                onGameLoad.Start();
+            };
         }
 
-        private static void onLoad(EventArgs args)
+        private static void Game_OnGameLoad()
         {
-
             Game.PrintChat("Tryndamere - Sharp by DeTuKs");
 
             try
             {
-
                 Config = new Menu("Trynd - SharpSwrod", "Tryndamere", true);
+
                 //Orbwalker
                 Config.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
-                Trynd.orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalker"));
+                Trynd.Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalker"));
+
                 //TS
-                var TargetSelectorMenu = new Menu("Target Selector", "Target Selector");
-                SimpleTs.AddToMenu(TargetSelectorMenu);
-                Config.AddSubMenu(TargetSelectorMenu);
+                var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
+                TargetSelector.AddToMenu(targetSelectorMenu);
+                Config.AddSubMenu(targetSelectorMenu);
+
                 //Combo
                 Config.AddSubMenu(new Menu("Combo Sharp", "combo"));
                 Config.SubMenu("combo").AddItem(new MenuItem("comboItems", "Use Items")).SetValue(true);
                 Config.SubMenu("combo").AddItem(new MenuItem("useW", "Use W")).SetValue(true);
                 Config.SubMenu("combo").AddItem(new MenuItem("useE", "Use E")).SetValue(true);
                 Config.SubMenu("combo").AddItem(new MenuItem("QonHp", "Q on % hp")).SetValue(new Slider(25, 100, 0));
-               // Config.SubMenu("combo").AddItem(new MenuItem("useR", "Use R on %")).SetValue(new Slider(25, 100, 0));
+                // Config.SubMenu("combo").AddItem(new MenuItem("useR", "Use R on %")).SetValue(new Slider(25, 100, 0));
 
                 //LastHit
                 Config.AddSubMenu(new Menu("LastHit Sharp", "lHit"));
-               
+
                 //LaneClear
                 Config.AddSubMenu(new Menu("LaneClear Sharp", "lClear"));
-               
-               
+
                 //Extra
                 Config.AddSubMenu(new Menu("Extra Sharp", "extra"));
 
                 //Debug
                 Config.AddSubMenu(new Menu("Debug", "debug"));
-                Config.SubMenu("debug").AddItem(new MenuItem("db_targ", "Debug Target")).SetValue(new KeyBind('T', KeyBindType.Press, false));
+                Config.SubMenu("debug")
+                    .AddItem(new MenuItem("db_targ", "Debug Target"))
+                    .SetValue(new KeyBind('T', KeyBindType.Press));
 
 
                 Config.AddToMainMenu();
-                Drawing.OnDraw += onDraw;
-                Game.OnGameUpdate += OnGameUpdate;
-
-                GameObject.OnCreate += OnCreateObject;
-                GameObject.OnDelete += OnDeleteObject;
-                Obj_AI_Base.OnProcessSpellCast += OnProcessSpell;
+                Drawing.OnDraw += Drawing_OnDraw;
+                Game.OnGameUpdate += Game_OnGameUpdate;
             }
             catch
             {
-                Game.PrintChat("Oops. Something went wrong with Yasuo- Sharpino");
+                Game.PrintChat("Oops. Something went wrong with Tryndamere - Sharp");
             }
-
         }
 
-        private static void OnGameUpdate(EventArgs args)
+        private static void Game_OnGameUpdate(EventArgs args)
         {
-
-            if (Trynd.orbwalker.ActiveMode.ToString() == "Combo")
+            if (Trynd.Orbwalker.ActiveMode.ToString() == "Combo")
             {
-              //  Console.WriteLine("emm");
-                if(Trynd.E.IsReady())
-                    target = SimpleTs.GetTarget(950, SimpleTs.DamageType.Physical);
+                // Console.WriteLine("emm");
+                if (Trynd.E.IsReady())
+                {
+                    Target = TargetSelector.GetTarget(950, TargetSelector.DamageType.Physical);
+                }
                 else if (Trynd.W.IsReady())
-                    target = SimpleTs.GetTarget(450, SimpleTs.DamageType.Physical);
+                {
+                    Target = TargetSelector.GetTarget(450, TargetSelector.DamageType.Physical);
+                }
                 else
-                    target = SimpleTs.GetTarget(250, SimpleTs.DamageType.Physical);
+                {
+                    Target = TargetSelector.GetTarget(250, TargetSelector.DamageType.Physical);
+                }
 
-                Trynd.doCombo(target);
+                Trynd.DoCombo(Target);
             }
 
-            if (Trynd.orbwalker.ActiveMode.ToString() == "Mixed")
+            if (Trynd.Orbwalker.ActiveMode.ToString() == "Mixed")
             {
-               
+                // Hmm..
             }
 
-            if (Trynd.orbwalker.ActiveMode.ToString() == "LaneClear")
+            if (Trynd.Orbwalker.ActiveMode.ToString() == "LaneClear")
             {
-                
+                // Hmm..
             }
 
 
-            if (Config.Item("harassOn").GetValue<bool>() && Trynd.orbwalker.ActiveMode.ToString() == "None")
+            if (Config.Item("harassOn").GetValue<bool>() && Trynd.Orbwalker.ActiveMode.ToString() == "None")
             {
-              
+                // Hmm..
             }
         }
 
-        private static void onDraw(EventArgs args)
+        private static void Drawing_OnDraw(EventArgs args)
         {
             Drawing.DrawCircle(Trynd.Player.Position, Trynd.E.Range, Color.Blue);
         }
-
-        private static void OnCreateObject(GameObject sender, EventArgs args)
-        {
-          
-
-        }
-
-        private static void OnDeleteObject(GameObject sender, EventArgs args)
-        {
-          
-        }
-
-        public static void OnProcessSpell(LeagueSharp.Obj_AI_Base obj, LeagueSharp.GameObjectProcessSpellCastEventArgs arg)
-        {
-
-
-           
-        }
-
-
-
-
     }
 }
