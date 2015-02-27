87701bf57ac358849a377dcd2bc31d5cdcdf1698
RoachxD <roachxxxx@gmail.com>
2015-02-27 13:31:07 +0200

    "What is broken can be reforged!"  - Updated for the Latest L# Version.  - Re-indented the whole Assembly.  - Removed Unused References.  - Removed Unused Functions/Events and Snippets of Code.  - Improved and Cleaned the whole Assembly including LxOrbwalker.

--- RivenSharpV2/Program.cs
+++ RivenSharpV2/Program.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
-namespace RivenSharp
+﻿namespace RivenSharpV2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             new RivenSharp();
         }
